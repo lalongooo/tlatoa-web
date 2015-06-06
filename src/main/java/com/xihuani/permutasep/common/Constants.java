1b8b81972a5a7e5cf8645caa03b3fa6dcc72a8b7
Angel Ricardo Vega Hernandez <angelricardo.uthh@gmail.com>
2015-06-06 00:28:47 -0500

    Services permuta sep - Yo Angel Ricardo Vega declaro firmente alejarme de las ocasiones de pecar, confio me perdonaras por tu infinita misecordia. AMEN

---
+++ src/main/java/com/xihuani/permutasep/common/Constants.java
@@ -0,0 +1,11 @@
+package com.xihuani.permutasep.common;
+
+public class Constants {
+	
+	public class View {
+		public final static String TLATOA_HOME = "manager/sentence";
+		public final static String TLATOA_RESOURCE = "resource";
+		public final static String TLATOA_HOME_JSP = "tlatoa";
+	}
+
+}
